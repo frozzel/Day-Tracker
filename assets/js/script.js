c0ad5bd265fd760f64515efaec69ea31bb761743
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-09-22 20:27:22 -0400

    Calling it quits all basic are here

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,17 @@
 ///////// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 var rootEl = $('.currentday')
 var stampEl = $('.timeStamp')
-var get5amEl = document.getElementById('8AM')
+$DescEl = $('description')
+// var get5amEl = document.getElementById('8AM')
 $backgroundEl= $('#8AM, #9AM, #10AM, #11AM, #12PM, #1PM, #2PM, #3PM, #4PM, #5PM')
 ///test 2//
 var timenow = new Date();
 var timeSplit = timenow.toString().split('2022')[0];
-var TextArea = document.getElementById('8AM')
-console.log(timenow)
-console.log(timeSplit)
+console.log($backgroundEl)
 
 
-stampEl.text(timeSplit+ ('')+ getLocaltime())
+
+stampEl.text(timeSplit)
 rootEl.append(timeSplit)
 
 /////// Time ////////// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
@@ -24,24 +24,36 @@ function getLocaltime(){
 console.log(getLocaltime())
 ////// ^^^^^^^^^^^ fire now how to use it //// 
 
-if (getLocaltime() < "06:00:00 PM" && getLocaltime() < "11:59:00 PM"){
-    $($backgroundEl).addClass('past col-8') 
-    console.log("hell ya")
-} else if (getLocaltime() > "12:00:00 AM" && getLocaltime() < "07:00:00 AM" ){
-    $($backgroundEl).addClass('future col-8') 
-    console.log("hell ya!!")
+///// Add Buttons////////////// https://www.plus2net.com/jquery/event-click.php
+$(document).ready(function(){
+    $('.saveBtn').click(function(){
+        var userText = $(this).siblings(".description").val();
+        var whenDesc = $(this).parent().attr("id");
+        localStorage.setItem(whenDesc, userText)
+        console.log(whenDesc, userText)
+    })
+})
+
+/////////// Keep Text Saves in Case of refresh////////////
+$('#8AM .description').val(localStorage.getItem('8AM'))
+$('#9AM .description').val(localStorage.getItem('9AM'))
+////// https://stackoverflow.com/questions/40791207/setting-and-getting-localstorage-with-jquery  //////
+
+///////////////////////Set Background ?//////////////
+if (getLocaltime() >= "8:00"){
+    $('#8AM').addClass('present')
+}else if (getLocaltime() < "08:00:00 AM" ){
+    $('#8AM').addClass('future') 
+    console.log("!!hell ya!!")
+} else if (getLocaltime() < "09:00:00 AM"){
+    $('#8AM').addClass('past') 
 }
-//// 04#6 ref for calls /////^^^^ pickup tomarrow/////
 
-// if (getLocaltime()< "09:00:00 AM"){
-//     get5amEl.className = 'past col-8'
-
-// } else if (getLocaltime()> "08:00:00 AM"){
-//     // get5amEl.className = 'future col-8';
-//     $($backgroundEl).addClass('future col-8') 
-//     console.log($backgroundEl)
-
-// }
-
-
-
+if (getLocaltime() >= "9:00"){
+    $('#9AM').addClass('present')
+}else if (getLocaltime() > "10:00:00" ){
+    $('#9AM').addClass('future') 
+    console.log("!!hell ya!!")
+} else if (getLocaltime() < "09:00:00 AM"){
+    $('#9AM').addClass('past') 
+}
