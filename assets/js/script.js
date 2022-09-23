0c3928831f8fd7c5b973bda3c1f885f47bceda68
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-09-22 21:20:08 -0400

    time to submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,16 @@
-// see unit 6 &1 & 10 to make text and save to screen///
-
 ///////////// Add time the easy waay ////////////
 ///////// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 var rootEl = $('.currentday')
 var stampEl = $('.timeStamp')
-$DescEl = $('description')
-// var get5amEl = document.getElementById('8AM')
-$backgroundEl= $('#8AM, #9AM, #10AM, #11AM, #12PM, #1PM, #2PM, #3PM, #4PM, #5PM')
-///test 2//
 var timenow = new Date();
 var timeSplit = timenow.toString().split('2022')[0];
-console.log($backgroundEl)
-
-
-
 stampEl.text(timeSplit)
 rootEl.append(timeSplit)
 
 /////// Time ////////// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
 function getLocaltime(){
     return new Date().toLocaleTimeString();
-}
-console.log(getLocaltime())
-////// ^^^^^^^^^^^ fire now how to use it //// 
+} 
 
 ///// Add Buttons////////////// https://www.plus2net.com/jquery/event-click.php
 $(document).ready(function(){
@@ -44,7 +32,6 @@ if (getLocaltime() >= "8:00"){
     $('#8AM').addClass('present')
 }else if (getLocaltime() < "08:00:00 AM" ){
     $('#8AM').addClass('future') 
-    console.log("!!hell ya!!")
 } else if (getLocaltime() < "09:00:00 AM"){
     $('#8AM').addClass('past') 
 }
@@ -53,7 +40,6 @@ if (getLocaltime() >= "9:00"){
     $('#9AM').addClass('present')
 }else if (getLocaltime() > "10:00:00" ){
     $('#9AM').addClass('future') 
-    console.log("!!hell ya!!")
 } else if (getLocaltime() < "09:00:00 AM"){
     $('#9AM').addClass('past') 
 }
